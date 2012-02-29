ebe658887fb09ce5d9ffd01155abdae1d7164f0c
Avi Tzurel <avi@kensodev.com>
2012-03-01 00:33:17 +0200

    Tests pass with the change to dalli

--- spec/test_app/config/environments/test.rb
+++ spec/test_app/config/environments/test.rb
@@ -13,7 +13,7 @@ TestApp::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-  config.cache_store = :mem_cache_store
+  config.cache_store = :dalli_store, '127.0.0.1'
 
   # Raise exceptions instead of rendering exception templates
   config.action_dispatch.show_exceptions = false
