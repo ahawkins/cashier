60ac963237b586fe61091c15b040ef3c2a2af3f3
Avi Tzurel <avi@kensodev.com>
2012-03-01 19:11:44 +0200

    Redis dependencies, changed the specs to fit the new context

--- cashier.gemspec
+++ cashier.gemspec
@@ -29,4 +29,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'guard'
   s.add_development_dependency 'guard-rspec'
   s.add_development_dependency 'growl'
+  s.add_development_dependency 'hiredis', '~> 0.3.1'
+  s.add_development_dependency 'redis', '~> 2.2.0'
 end
--- spec/lib/cashier_spec.rb
+++ spec/lib/cashier_spec.rb
@@ -1,88 +1,93 @@
 require 'spec_helper'
 
 describe "Cashier" do
-  subject { Cashier }
-  let(:cache) { Rails.cache }
-
-  describe "#store_fragment" do
-    it "should write the tag to the cache" do
-      subject.store_fragment('fragment-key', 'dashboard')
-
-      cache.fetch('dashboard').should eql(['fragment-key'])
-    end
-
-    it "should store the tag for book keeping" do
-      subject.store_fragment('fragment-key', 'dashboard', 'settings')
-      cache.fetch(Cashier::CACHE_KEY).should eql(%w(dashboard settings))
-    end
+  context "Redis as the cache adapter" do
+    
   end
+  context "Rails.cache as the adapter" do
+    subject { Cashier }
+    let(:cache) { Rails.cache }
 
-  describe "#expire" do
-    before do
-      subject.store_fragment('fragment-key', 'dashboard')
+    describe "#store_fragment" do
+      it "should write the tag to the cache" do
+        subject.store_fragment('fragment-key', 'dashboard')
+
+        cache.fetch('dashboard').should eql(['fragment-key'])
+      end
+
+      it "should store the tag for book keeping" do
+        subject.store_fragment('fragment-key', 'dashboard', 'settings')
+        cache.fetch(Cashier::CACHE_KEY).should eql(%w(dashboard settings))
+      end
     end
 
-    it "should remove delete the fragment key" do
-      subject.expire('dashboard')
-      Rails.cache.fetch('fragment-key').should be_nil
+    describe "#expire" do
+      before do
+        subject.store_fragment('fragment-key', 'dashboard')
+      end
+
+      it "should remove delete the fragment key" do
+        subject.expire('dashboard')
+        Rails.cache.fetch('fragment-key').should be_nil
+      end
+
+      it "should remove the tag" do 
+        subject.expire('dashboard')
+        Rails.cache.fetch('dashboard').should be_nil
+      end
+
+      it "should remove the tag from the list of tracked tags"  do
+        subject.expire('dashboard')
+        Rails.cache.fetch(Cashier::CACHE_KEY).should eql([])
+      end
     end
 
-    it "should remove the tag" do
-      subject.expire('dashboard')
-      Rails.cache.fetch('dashboard').should be_nil
+    describe "#tags" do
+      it "should return a list of active tags" do
+        subject.store_fragment('key1', 'dashboard')
+        subject.store_fragment('key2', 'settings')
+        subject.store_fragment('key3', 'email')
+
+        subject.tags.should eql(%w(dashboard settings email))
+      end
     end
 
-    it "should remove the tag from the list of tracked tags"  do
-      subject.expire('dashboard')
-      Rails.cache.fetch(Cashier::CACHE_KEY).should eql([])
-    end
-  end
+    describe '#clear' do
+      before(:each) do
+        subject.store_fragment('key1', 'dashboard')
+        subject.store_fragment('key2', 'settings')
+        subject.store_fragment('key3', 'email')
+      end
 
-  describe "#tags" do
-    it "should return a list of active tags" do
-      subject.store_fragment('key1', 'dashboard')
-      subject.store_fragment('key2', 'settings')
-      subject.store_fragment('key3', 'email')
+      it "should expire all tags" do
+        subject.should_receive(:expire).with('dashboard','settings','email')
+        subject.clear
+      end
 
-      subject.tags.should eql(%w(dashboard settings email))
-    end
-  end
-
-  describe '#clear' do
-    before(:each) do
-      subject.store_fragment('key1', 'dashboard')
-      subject.store_fragment('key2', 'settings')
-      subject.store_fragment('key3', 'email')
+      it "should clear the list of tracked tags" do
+        subject.clear
+        cache.fetch(Cashier::CACHE_KEY).should be_nil
+      end
     end
 
-    it "should expire all tags" do
-      subject.should_receive(:expire).with('dashboard','settings','email')
-      subject.clear
+    describe '#keys' do
+      it "should return an array of all the tracked keys" do
+        subject.store_fragment('key1', 'dashboard')
+        subject.store_fragment('key2', 'settings')
+        subject.store_fragment('key3', 'email')
+
+        subject.keys.should eql(%w(key1 key2 key3))
+      end
     end
 
-    it "should clear the list of tracked tags" do
-      subject.clear
-      cache.fetch(Cashier::CACHE_KEY).should be_nil
-    end
-  end
+    describe '#keys_for' do
+      it "should return an array of all the keys for the tag" do
+        subject.store_fragment('key1', 'dashboard')
+        subject.store_fragment('key2', 'dashboard')
+        subject.store_fragment('key3', 'dashboard')
 
-  describe '#keys' do
-    it "should return an array of all the tracked keys" do
-      subject.store_fragment('key1', 'dashboard')
-      subject.store_fragment('key2', 'settings')
-      subject.store_fragment('key3', 'email')
-
-      subject.keys.should eql(%w(key1 key2 key3))
-    end
-  end
-
-  describe '#keys_for' do
-    it "should return an array of all the keys for the tag" do
-      subject.store_fragment('key1', 'dashboard')
-      subject.store_fragment('key2', 'dashboard')
-      subject.store_fragment('key3', 'dashboard')
-
-      subject.keys_for('dashboard').should eql(%w(key1 key2 key3))
+        subject.keys_for('dashboard').should eql(%w(key1 key2 key3))
+      end
     end
   end
 end
