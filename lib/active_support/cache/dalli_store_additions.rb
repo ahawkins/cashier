d2b171cf5ec7ef928ba7ffc86431c26e261e057e
Avi Tzurel <avi@kensodev.com>
2012-03-20 22:18:21 +0200

    No need to call that in fetch, only in write

--- lib/active_support/cache/dalli_store_additions.rb
+++ lib/active_support/cache/dalli_store_additions.rb
@@ -10,14 +10,6 @@ module ActiveSupport
     # DalliStore implements the Strategy::LocalCache strategy which implements
     # an in memory cache inside of a block.
     class DalliStore < Store
-      def fetch_with_tags(key, options = {})
-        tags = options.delete(:tag)
-        Cashier.store_fragment(key, tags) if tags
-
-        fetch_without_tags(key, options)
-      end
-      alias_method_chain :fetch, :tags
-
       def write_with_tags(key, value, options = {})
         tags = options.delete(:tag)
         Cashier.store_fragment(key, tags) if tags
