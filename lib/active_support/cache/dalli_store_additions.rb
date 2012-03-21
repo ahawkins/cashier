507e728d788dddd06f0055e0ddb482152c19f684
Avi Tzurel <avi@kensodev.com>
2012-03-21 20:31:47 +0200

    on_cache_write and on_cache_delete callbacks

--- lib/active_support/cache/dalli_store_additions.rb
+++ lib/active_support/cache/dalli_store_additions.rb
@@ -12,17 +12,22 @@ module ActiveSupport
     # an in memory cache inside of a block.
     class DalliStore < Store
       def write_with_tags(key, value, options = {})
+        Cashier::Addons::Plugins.call_plugin_method(:on_cache_write, key)
+
         tags = options.delete(:tag)
         Cashier.store_fragment(key, tags) if tags
+
         write_without_tags(key, value, options)
       end
       alias_method_chain :write, :tags
 
 
       def delete_with_tags(key, options = nil)
-        Cashier::Addons::Plugins.call_plugin_method(:on_delete_key, key)
+        Cashier::Addons::Plugins.call_plugin_method(:on_cache_delete, key)
+
         delete_without_tags(key, options)
       end
+
       alias_method_chain :delete, :tags
     end
   end
--- spec/lib/addons/plugins_spec.rb
+++ spec/lib/addons/plugins_spec.rb
@@ -1,43 +1,50 @@
 require 'spec_helper'
 
 describe Cashier::Addons::Plugins do
-    subject { Cashier::Addons::Plugins }
-    let(:cashier) { Cashier }
+  subject { Cashier::Addons::Plugins }
+  let(:cashier) { Cashier }
 
-    before(:each) do
-      Cashier::Addons::Adapters.adapter = :cache_store
-    end
+  before(:each) do
+    Cashier::Addons::Adapters.adapter = :cache_store
+  end
 
-    it "should have add_plugin method" do
-      subject.respond_to?(:add_plugin).should be_true
-    end
+  it "should have add_plugin method" do
+    subject.respond_to?(:add_plugin).should be_true
+  end
 
-    it "should have plugins method that returns an array" do
-      subject.plugins.should == []
-    end
+  it "should have plugins method that returns an array" do
+    subject.plugins.should == []
+  end
 
-    it "should be able to add a plugin" do
-      subject.add_plugin(DummyPlugin)
-      subject.plugins.include?(DummyPlugin).should be_true
-    end
+  it "should be able to add a plugin" do
+    subject.add_plugin(DummyPlugin)
+    subject.plugins.include?(DummyPlugin).should be_true
+  end
 
-    it "should raise a callback when I call store_fragment" do
-      subject.should_receive(:call_plugin_method).with(:on_store_fragment, "foo", ["bar"])
-      cashier.store_fragment("foo", "bar")
-    end
+  it "should raise a callback when I call store_fragment" do
+    subject.should_receive(:call_plugin_method).with(:on_store_fragment, "foo", ["bar"])
+    Rails.cache.should_receive(:write).at_least(:twice)
+    
+    cashier.store_fragment("foo", "bar")
+  end
 
-    it "should raise a callback method when I call clear" do
-      subject.should_receive(:call_plugin_method).at_least(:once)
-      cashier.clear
-    end
+  it "should raise a callback method when I call clear" do
+    subject.should_receive(:call_plugin_method).at_least(:once)
+    cashier.clear
+  end
 
-    it "should raise a callback method when I call expire" do
-      subject.should_receive(:call_plugin_method).at_least(:once)
-      cashier.expire("some_tag")
-    end
+  it "should raise a callback method when I call expire" do
+    subject.should_receive(:call_plugin_method).at_least(:once)
+    cashier.expire("some_tag")
+  end
 
-    it "should raise a callback when I call Rails.cache.delete" do
-      Cashier::Addons::Plugins.should_receive(:call_plugin_method).with(:on_delete_key, "some_key")
-      Rails.cache.delete("some_key")
-    end
+  it "should raise a callback when I call Rails.cache.delete" do
+    subject.should_receive(:call_plugin_method).with(:on_cache_delete, "some_key")
+    Rails.cache.delete("some_key")
+  end
+
+  it "should rails a callback when I call Rails.cache.write" do
+    subject.should_receive(:call_plugin_method).with(:on_cache_write, "foo")
+    Rails.cache.write("foo", "bar")
+  end
 end
