bb8c112e3cf5c83721a8b7d13fd28eec3027aed7
Avi Tzurel <avi@kensodev.com>
2012-03-21 02:45:29 +0200

    Added plugin support for Rails.cache.delete, added color to the specs

--- lib/cashier.rb
+++ lib/cashier.rb
@@ -59,7 +59,7 @@ module Cashier
 
   def call_plugin_method(method_name, *args)
     plugins.each do |plugin|
-      plugin.send(method_name, args) if plugin.respond_to(method_name)
+      plugin.send(method_name, args) if plugin.respond_to?(method_name)
     end
   end
 
