8ac3725a2351e966b7267e6c24f90d89f14a9181
John Brandy & Tarik Ansari <pair+john+tarik@rally.org>
2012-06-06 17:26:17 -0700

    Move time of loading of the ApplicationController
    Previously the railtie was requiring the application_controller on load of `action_controller`, which failed with certain rails applications. `before_configuration` comes later and ensure the ApplicationController already loaded.

--- lib/cashier/railtie.rb
+++ lib/cashier/railtie.rb
@@ -5,8 +5,8 @@ module Cashier
     initializer "cashier.active_support.cache.instrumentation" do |app|
       ActiveSupport::Cache::Store.instrument = true
 
-      ActiveSupport.on_load(:action_controller) do
-        require 'cashier/application_controller'
+      ActiveSupport.on_load(:before_configuration) do
+        require "cashier/application_controller"
       end
     end
   end
