641e1f41ab5dcbcea83acc7d169754fc93ef603b
John Brandy & Tarik Ansari <pair+john+tarik@rally.org>
2012-06-06 17:43:58 -0700

    Include methods on ActionController instead.

--- lib/cashier/railtie.rb
+++ lib/cashier/railtie.rb
@@ -5,8 +5,10 @@ module Cashier
     initializer "cashier.active_support.cache.instrumentation" do |app|
       ActiveSupport::Cache::Store.instrument = true
 
-      ActiveSupport.on_load(:before_configuration) do
-        require "cashier/application_controller"
+      ActiveSupport.on_load(:action_controller) do
+        require "cashier/action_controller_methods"
+
+        ::ActionController::Base.send(:include, Cashier::ActionControllerMethods)
       end
     end
   end
