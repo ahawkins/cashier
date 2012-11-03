b07dfbcde01e3cca6eaa2fedd1d68946bd64f4f7
twinturbo <me@broadcastingadam.com>
2012-11-03 16:14:37 +0100

    Prefer class << self

--- lib/cashier.rb
+++ lib/cashier.rb
@@ -1,120 +1,120 @@
 module Cashier
 
-  extend self
-
   CACHE_KEY = 'cashier-tags'
+  
+  class << self
 
-  # Public: whether the module will perform caching or not. this is being set in the application layer .perform_caching configuration
-  #
-  # Examples
-  #
-  #   Cashier.perform_caching?
-  #   # => true
-  #
-  def perform_caching?
-    ::ApplicationController.perform_caching
-  end
-
-  # Public: store a fragment with an array of tags for this fragment.
-  #
-  # fragment - cached fragment.
-  # tags - array of tags you want to assign this fragments.
-  #
-  # Examples
-  #
-  #   Cachier.store_fragment('foo', 'tag1', 'tag2', 'tag3')
-  #
-  def store_fragment(fragment, *tags)
-    return unless perform_caching?
-
-    ActiveSupport::Notifications.instrument("store_fragment.cashier", :data => [fragment, tags]) do
-      tags.each do |tag|
-        # store the fragment
-        adapter.store_fragment_in_tag(fragment, tag)
-      end
-
-       # now store the tag for book keeping
-      adapter.store_tags(tags)
+    # Public: whether the module will perform caching or not. this is being set in the application layer .perform_caching configuration
+    #
+    # Examples
+    #
+    #   Cashier.perform_caching?
+    #   # => true
+    #
+    def perform_caching?
+      ::ApplicationController.perform_caching
     end
-  end
 
-  # Public: expire tags. expiring the keys 'assigned' to the tags you expire and removes the tags from the tags list
-  # 
-  # tags - array of tags to expire.
-  # 
-  # Examples
-  #
-  #   Cashier.expire('tag1', 'tag2')
-  # 
-  def expire(*tags)
-    return unless perform_caching?
+    # Public: store a fragment with an array of tags for this fragment.
+    #
+    # fragment - cached fragment.
+    # tags - array of tags you want to assign this fragments.
+    #
+    # Examples
+    #
+    #   Cachier.store_fragment('foo', 'tag1', 'tag2', 'tag3')
+    #
+    def store_fragment(fragment, *tags)
+      return unless perform_caching?
 
-    ActiveSupport::Notifications.instrument("expire.cashier", :data => tags) do
-      # delete them from the cache
-      tags.each do |tag|
-        fragment_keys = adapter.get_fragments_for_tag(tag)
-
-        fragment_keys.each do |fragment_key|
-          Rails.cache.delete(fragment_key)
+      ActiveSupport::Notifications.instrument("store_fragment.cashier", :data => [fragment, tags]) do
+        tags.each do |tag|
+          # store the fragment
+          adapter.store_fragment_in_tag(fragment, tag)
         end
 
-        adapter.delete_tag(tag)
+         # now store the tag for book keeping
+        adapter.store_tags(tags)
       end
-
-      # now remove them from the list
-      # of stored tags
-      adapter.remove_tags(tags)
     end
-  end
 
-  # Public: returns the array of tags stored in the tags store.
-  #
-  #
-  # Examples
-  #
-  #   Cashier.tags
-  #   # => ['tag1', 'tag2']
-  #
-  def tags
-    adapter.tags
-  end
+    # Public: expire tags. expiring the keys 'assigned' to the tags you expire and removes the tags from the tags list
+    # 
+    # tags - array of tags to expire.
+    # 
+    # Examples
+    #
+    #   Cashier.expire('tag1', 'tag2')
+    # 
+    def expire(*tags)
+      return unless perform_caching?
 
-  # Public: clears the tags.
-  #
-  #
-  # Examples
-  #
-  #   Cashier.clear
-  #
-  def clear
-    ActiveSupport::Notifications.instrument("clear.cashier") do
-      adapter.clear
+      ActiveSupport::Notifications.instrument("expire.cashier", :data => tags) do
+        # delete them from the cache
+        tags.each do |tag|
+          fragment_keys = adapter.get_fragments_for_tag(tag)
+
+          fragment_keys.each do |fragment_key|
+            Rails.cache.delete(fragment_key)
+          end
+
+          adapter.delete_tag(tag)
+        end
+
+        # now remove them from the list
+        # of stored tags
+        adapter.remove_tags(tags)
+      end
     end
-  end
 
-  # Public: get all the keys names as an array.
-  #
-  #
-  # Examples
-  #
-  #   Cachier.keys
-  #   # => ['key1', 'key2', 'key3']
-  #
-  def keys
-    adapter.keys
-  end
+    # Public: returns the array of tags stored in the tags store.
+    #
+    #
+    # Examples
+    #
+    #   Cashier.tags
+    #   # => ['tag1', 'tag2']
+    #
+    def tags
+      adapter.tags
+    end
 
-  # Public: get all the keys for a specific tag as an array.
-  #
-  #
-  # Examples
-  #
-  #   Cashier.tags_for('tag1')
-  #   # => ['key1', 'key2', 'key3']
-  #
-  def keys_for(tag)
-    adapter.get_fragments_for_tag(tag)
-  end
+    # Public: clears the tags.
+    #
+    #
+    # Examples
+    #
+    #   Cashier.clear
+    #
+    def clear
+      ActiveSupport::Notifications.instrument("clear.cashier") do
+        adapter.clear
+      end
+    end
+
+    # Public: get all the keys names as an array.
+    #
+    #
+    # Examples
+    #
+    #   Cachier.keys
+    #   # => ['key1', 'key2', 'key3']
+    #
+    def keys
+      adapter.keys
+    end
+
+    # Public: get all the keys for a specific tag as an array.
+    #
+    #
+    # Examples
+    #
+    #   Cashier.tags_for('tag1')
+    #   # => ['key1', 'key2', 'key3']
+    #
+    def keys_for(tag)
+      adapter.get_fragments_for_tag(tag)
+    end
 
     # Public: adapter which is used by cashier.
     #
@@ -145,6 +145,7 @@ module Cashier
     def adapter=(cache_adapter)
       @@adapter = cache_adapter
     end
+  end
 end
 
 require 'rails'
