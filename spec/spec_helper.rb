5ed9bd94ec06d4e95e9964c1000ec9b0ede50a28
twinturbo <me@broadcastingadam.com>
2013-02-26 16:35:24 +0100

    Don't start own redis process

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,33 +16,12 @@ require 'rspec/rails'
 require 'fileutils'
 
 RSpec.configure do |config|
-  # ==========================> Redis test configuration
-  REDIS_PID = Rails.root.join 'tmp', 'pids', 'redis.pid'
-
-  FileUtils.mkdir_p Rails.root.join 'tmp', 'pids'
-  FileUtils.mkdir_p Rails.root.join 'tmp', 'cache'
-
   config.before(:suite) do
-    redis_options = {
-      "daemonize"     => 'yes',
-      "pidfile"       => REDIS_PID,
-      "port"          => 6397,
-      "dir"           => Rails.root.join('tmp', 'cache'),
-    }.map { |k, v| "#{k} #{v}" }.join('\n')
-
-    `echo '#{redis_options}' | redis-server -`
-
-    sleep 0.25
-
-    Cashier::Adapters::RedisStore.redis = Redis.new(:host => '127.0.0.1', :port => 6397)
+    Cashier::Adapters::RedisStore.redis = Redis.new :host => '127.0.0.1'
   end
 
   config.before(:each) do
     Cashier::Adapters::RedisStore.redis.flushdb
     Rails.cache.clear
-  end
-
-  config.after :suite do
-    Process.kill "TERM", File.read(REDIS_PID).to_i
   end
 end
